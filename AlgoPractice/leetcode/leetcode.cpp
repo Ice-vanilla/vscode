73103186332c8f42fa2f44f83e37a412987b5037
Ice-vanilla <1011694931@qq.com>
2021-03-08 14:17:00 +0800

    record something

---
+++ AlgoPractice/leetcode/leetcode.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+};
+
+int main()
+{
+    Solution s;
+
+    clock_t start = clock();
+
+    clock_t end = clock();
+
+    cout << "time: " << (end - start) * 1000 / CLOCKS_PER_SEC << "ms\n";
+
+    return 0;
+}
